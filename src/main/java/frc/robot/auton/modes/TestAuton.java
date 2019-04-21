9f66f8dcf2fedf2500915ae1abe97aff11b072ef
HarrelsonJ <34192484+HarrelsonJ@users.noreply.github.com>
2019-04-20 22:19:14 -0400

    Worked on implementing more action auton framwork

--- src/main/java/frc/robot/auton/modes/TestAuton.java
+++ src/main/java/frc/robot/auton/modes/TestAuton.java
@@ -3,6 +3,9 @@ package frc.robot.auton.modes;
 import frc.robot.auton.AutonBase;
 import frc.robot.auton.actions.*;
 
+import java.util.Arrays;
+
+import edu.wpi.first.wpilibj.Timer;
 import frc.lib.trajectory.TrajectoryGenerator;
 import frc.lib.trajectory.TrajectoryGenerator.Trajectories;
 
@@ -17,6 +20,19 @@ public class TestAuton extends AutonBase {
     //Fill this with actions
     @Override
     protected void routine() {
-        runAction(new TestAction(1.0));
+        System.out.println("First action started at: "  + Timer.getFPGATimestamp());
+
+        runAction(new TestAction(1.0));//Total wait time of 1 second
+
+        System.out.println("First action completed and second action started at: " + Timer.getFPGATimestamp());
+
+        runAction(new ParallelAction(Arrays.asList(new TestAction(1.0), new TestAction(2.0))));//Total wait time of two seconds
+
+        System.out.println("Second action completed and third action started at: " + Timer.getFPGATimestamp());
+
+        runAction(new SeriesAction(Arrays.asList(new TestAction(1.0), new TestAction(2.0))));//Total wait time of three seconds
+        
+        System.out.println("Third action completed at: " + Timer.getFPGATimestamp());
+
     }
 }
